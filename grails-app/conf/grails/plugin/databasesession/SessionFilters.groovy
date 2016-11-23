b4cbfd33849bad4d7d55ebcff4fbb0485ab4a203
jerryb <jerry@jukinmedia.com>
2016-11-23 07:09:25 -0800

    make SessionFilter aware of flags that disable it; do not try to do work if the response is committed

--- grails-app/conf/grails/plugin/databasesession/SessionFilters.groovy
+++ grails-app/conf/grails/plugin/databasesession/SessionFilters.groovy
@@ -7,25 +7,45 @@ import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes
  */
 class SessionFilters {
 
-	def gormPersisterService
+    def gormPersisterService
 
-	def filters = {
+    def filters = {
 
-		flash(controller:'*', action:'*') {
+        flash(controller: '*', action: '*') {
+            afterView = { Exception e ->
+                if (e || request.getAttribute(GrailsApplicationAttributes.FLASH_SCOPE) == null) {
+                    return
+                }
 
-			afterView = { Exception e ->
-				if (request.getAttribute(GrailsApplicationAttributes.FLASH_SCOPE) == null) {
-					return
-				}
+                def enabled = databaseSessionEnabled(grailsApplication.config)
+                if (!enabled) {
+                    return
+                }
 
-				try {
-					// set the value to the key as a flag to retrieve it from the request
-					gormPersisterService.setAttribute(request.session.id, GrailsApplicationAttributes.FLASH_SCOPE, GrailsApplicationAttributes.FLASH_SCOPE)
-				}
-				catch (InvalidatedSessionException ise) {
-					// ignored
-				}
-			}
-		}
-	}
+                def alreadyCommitted = response.isCommitted()
+                if (alreadyCommitted) {
+                    return
+                }
+
+                try {
+                    // set the value to the key as a flag to retrieve it from the request
+                    gormPersisterService.setAttribute(request.session.id, GrailsApplicationAttributes.FLASH_SCOPE, GrailsApplicationAttributes.FLASH_SCOPE)
+                }
+                catch (InvalidatedSessionException ignored) {
+                    // ignored
+                }
+            }
+        }
+    }
+
+    @SuppressWarnings("GrMethodMayBeStatic")
+    private boolean databaseSessionEnabled(config) {
+        def pluginEnabled = config.grails.plugin.databasesession.enabled
+        pluginEnabled = pluginEnabled instanceof Boolean ? pluginEnabled : false
+
+        def flashStorageEnabled = config.grails.plugin.redisdatabasesession.storeFlashScopeWithRedis
+        flashStorageEnabled = flashStorageEnabled instanceof Boolean ? flashStorageEnabled : false
+
+        return pluginEnabled && flashStorageEnabled
+    }
 }
